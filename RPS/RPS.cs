e0aa3d66bb91d028a088c574d2c1c3eec37dedf8
Jesper-Andersson <ajesper00@gmail.com>
2022-12-15 22:35:10 +0200

    Basic Functionality

--- RPS/RPS.cs
+++ RPS/RPS.cs
@@ -1,39 +1,82 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace ConsoleApp1
 {
-    internal class RPS
+    static internal class Rps
     {
         static void Main(string[] args)
         {
+            int wins = 0;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Wins: " + wins);
+
+                int gameResult = GameLoop();
+
+                if (gameResult == 1) { wins++; } 
+                else if (gameResult == 2) {  }
+                else { wins--; }
+            }
+        }
+        static int GameLoop()
+        {
+            // 0 - rock
+            // 1 - scissors
+            // 2 - paper
+
+            string[] moves = { "Rock", "Scissors", "Paper" };
+
             var rand = new Random();
 
+            int choice = 0;
             bool validInput = false;
-            int choice;
 
             Console.WriteLine("Rock Paper Scissors");
-            Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
-
+            Console.WriteLine("0. Rock, 1. Scissors, 2. Paper \n");
 
             while (!validInput)
             {
-                string choiceInput = Console.ReadLine();
+                choice = int.Parse(Console.ReadLine());
 
-                if (choiceInput != null)
+                if (choice == 0 || choice == 1 || choice == 2)
                 {
-                    choice = int.Parse(choiceInput);
                     validInput = true;
                 }
                 else
                 {
                     validInput = false;
+                    Console.WriteLine("Invalid input, try again:");
                 }
             }
-            
-
-            
 
             int botChoice = rand.Next(0, 2);
+            
+            if (choice > botChoice)
+            {
+                Console.WriteLine("You Win!");
+                Console.WriteLine(moves[choice] + " beats " + moves[botChoice] + "!");
+                return 1;
+            }
+            else if (choice == botChoice)
+            {
+                Console.WriteLine("Draw.");
+                Console.WriteLine("You both picked" + moves[choice] + "!");
+                return 2;
+            }
+            else
+            {
+                Console.WriteLine("You Lose!");
+                Console.WriteLine(moves[botChoice] + " beats " + moves[choice] + "!");
+                return 0;
+            }
+
+            Console.WriteLine("Press any key to play again...");
+            Console.ReadLine();
+            
         }
     }
 }
