9e11b3b28d8da0f63f3f008bcb9d00aa46f0789d
jespe <jespe@DESKTOP-NSFFB7V>
2022-12-15 18:13:52 +0200

    Add project files.

---
+++ ConsoleApp1/RPS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class RPS
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            bool validInput = false;
+            int choice;
+
+            Console.WriteLine("Rock Paper Scissors");
+            Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
+
+
+            while (!validInput)
+            {
+                string choiceInput = Console.ReadLine();
+
+                if (choiceInput != null)
+                {
+                    choice = int.Parse(choiceInput);
+                    validInput = true;
+                }
+                else
+                {
+                    validInput = false;
+                }
+            }
+            
+
+            
+
+            int botChoice = rand.Next(0, 2);
+        }
+    }
+}
