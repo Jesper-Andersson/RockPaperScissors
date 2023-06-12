f4267b068168497dd62e387864ad76e4789e612a
Jesper-Andersson <ajesper00@gmail.com>
2023-06-13 01:36:07 +0300

    Basic Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -18,30 +18,51 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <conio.h>
 #include <iostream>
-/*	
-	0. Scissors
-	1. Rock	
-    2. Paper
-*/
 
 bool is_playing = true;
 
 int player_score = 0;
 int opponent_score = 0;
 
-int temp_score;
+int max_score;
 
 short player_input;
 
+std::string moves[] = { "Scissors", "Rock", "Paper" };
+
 void GameRound() {
+	std::cout << "Best of: " << max_score << "." << std::endl;
 	std::cout << "Player: " << player_score << std::endl << "Opponent: " << opponent_score << std::endl;
 
 	short selection = std::rand() % 3;
 
-	std::cout << selection;
+	std::cout << "0. Scissors" << std::endl << "1. Rock" << std::endl << "2. Paper" << std::endl;
 	
-	std::cin.get();
+	std::cout << "You choose: ";
+
+	std::cin >> player_input;  //TODO: SANITISE INPUT
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+
+	std::cout << std::endl;
+
+	//	0. Scissors, 1. Rock, 2. Paper	
+
+	if (player_input + 1 == selection || player_input - 2 == selection) {
+		opponent_score += 1;
+		std::cout << "Opponent Won Turn" << std::endl;
+	}
+	else if (player_input - 1 == selection || player_input + 2 == selection) {
+		player_score += 1;
+		std::cout << "You Won the Turn!" << std::endl;
+	} 
+	else{
+		std::cout << "Round Draw." << std::endl;
+	}
+
+	std::cout << "You: " << moves[player_input] << " vs Opponent: " << moves[selection] << std::endl; 
 
 	return;
 }
@@ -50,14 +71,32 @@ int main() {
 	//Sets the seed
 	std::srand(std::time(nullptr));
 
-	short scissors = 0;
-	short rock = 1;
-	short paper = 2;
+	std::cout << "Best of... ? (Max Score): ";
+
+	std::cin >> max_score; //TODO: SANITISE INPUT
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
 	
 	while (is_playing)
 	{
-		GameRound();
+		if (player_score < max_score && opponent_score < max_score) {
+			GameRound();
+		}
+		else {
+			is_playing = false;
+		}
 	}
+
+	if (player_score < opponent_score) {
+		std::cout << std::endl << "You Won the Game" << std::endl;
+	}
+	else {
+		std::cout << std::endl << "You Lost the Game" << std::endl;
+	}
+
+	std::cout << "Game Over.";
+
+	//std::cout << "Play Again?: [Y/N]"
 
 	return 0;
 }
